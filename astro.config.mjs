86a5ce351a5415889f05d686b46927b37b63ff06
Jon Coleman <jcoleman1388@gmail.com>
2023-01-19 22:37:36 -0500

    initial

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact()]
+});
