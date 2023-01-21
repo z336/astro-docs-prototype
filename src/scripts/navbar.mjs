6fccdedcec33f1c7541ba51b9be7d53c644d9be8
Jon Coleman <jcoleman1388@gmail.com>
2023-01-21 18:14:24 -0500

    functional nav

--- src/scripts/navbar.mjs
+++ src/scripts/navbar.mjs
@@ -19,3 +19,39 @@ nav.addEventListener('keyup', (e) => {
 });
 
 nav.insertBefore(burgerClone, container);
+
+// const nav = document.querySelector('.navbar');
+// const container = nav.querySelector('div');
+// const burgerClone = document
+//   .querySelector('#burger-template')
+//   .content.cloneNode(true);
+// // const svg = nav.querySelector("svg");
+
+// const button = burgerClone.querySelector('#menu');
+// button.addEventListener('click', (event) => {
+//   const isOpen = button.getAttribute('aria-expanded') === 'false';
+//   button.setAttribute('aria-expanded', isOpen);
+// });
+
+// // avoid DRY: disabling menu
+// const disableMenu = () => {
+//   button.setAttribute('aria-expanded', false);
+//   button.focus();
+// };
+
+// //  close on escape
+// nav.addEventListener('keyup', (event) => {
+//   if (event.code === 'Escape') {
+//     disableMenu();
+//   }
+// });
+
+// // close if clicked outside of event target
+// document.addEventListener('click', (event) => {
+//   const isClickInsideElement = nav.contains(event.target);
+//   if (!isClickInsideElement) {
+//     disableMenu();
+//   }
+// });
+
+// nav.insertBefore(burgerClone, container);
