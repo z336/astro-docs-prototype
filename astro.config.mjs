276c0e9fa9ac6534ac40c7970a043615978a8b1b
Jon Coleman <jcoleman1388@gmail.com>
2023-01-24 08:53:27 -0500

    images mostly set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import addClasses from './add-classes.mjs';
+import image from '@astrojs/image';
 import preact from '@astrojs/preact';
 import mdx from '@astrojs/mdx';
 import rehypeSlug from 'rehype-slug';
@@ -8,7 +9,11 @@ import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), mdx()],
+  integrations: [
+    preact(),
+    mdx(),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
+  ],
   markdown: {
     rehypePlugins: [
       rehypeSlug,
