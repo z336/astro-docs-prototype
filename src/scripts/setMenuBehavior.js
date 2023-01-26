909db00be676c8c6a93ad20f2382081f2aac6d5b
Jon Coleman <jcoleman1388@gmail.com>
2023-01-26 08:12:19 -0500

    fixed menu details behavior

--- src/scripts/setMenuBehavior.js
+++ src/scripts/setMenuBehavior.js
@@ -2,11 +2,15 @@ const menus = document.querySelectorAll('.menu');
 const currentNavPage = document.querySelector(
   `.navbar details a[aria-current="page"]`
 );
+const currentSideBarPage = document.querySelector(
+  `.sidebar details a[aria-current="page"]`
+);
 
 function updateDetailsAttribute() {
   if (window.innerWidth <= 1280) {
     menus.forEach((menu) => menu.removeAttribute('open'));
     currentNavPage.closest('details').setAttribute('open', '');
+    currentSideBarPage.closest('details').setAttribute('open', '');
   } else {
     menus.forEach((menu) => menu.setAttribute('open', ''));
   }
