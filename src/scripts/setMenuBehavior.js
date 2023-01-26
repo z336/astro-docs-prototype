847e90d0b9eba13d496f1dd922cbaa73fa5412e9
Jon Coleman <jcoleman1388@gmail.com>
2023-01-25 22:35:57 -0500

    fixed breadcrumbs

--- src/scripts/setMenuBehavior.js
+++ src/scripts/setMenuBehavior.js
@@ -3,6 +3,8 @@ const menus = document.querySelectorAll('.menu');
 function updateDetailsAttribute() {
   if (window.innerWidth <= 1280) {
     menus.forEach((menu) => menu.removeAttribute('open'));
+  } else {
+    menus.forEach((menu) => menu.setAttribute('open', ''));
   }
 }
 
