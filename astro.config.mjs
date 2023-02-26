b0a7ee0cf4d2b69f87e16edaff493b28e503c3a3
Jon Coleman <jcoleman1388@gmail.com>
2023-02-26 16:52:05 -0500

    small change to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,12 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
+// https://docs.astro.build/en/guides/markdown-content/
+// https://docs.astro.build/en/guides/images/
+
+// Shiki themes & languages
+// https://github.com/shikijs/shiki/blob/main/docs/themes.md
+// https://github.com/shikijs/shiki/blob/main/docs/languages.md
 export default defineConfig({
   integrations: [
     preact({ compat: true }),
@@ -31,14 +37,8 @@ export default defineConfig({
       ],
     ],
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'github-dark',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: [],
-      // Enable word wrap to prevent horizontal scrolling
       wrap: true,
     },
     extendDefaultPlugins: true,
